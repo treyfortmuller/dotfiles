9b04b7d90119738d78c5b8a00acf91c92a15da15
treyfortmuller <tfortmuller@berkeley.edu>
2018-12-25 00:58:30 -0800

    added desktop snap installs

---
+++ desktop_install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+snap install slack --classic
+snap install spotify
+
+# this might require a log out and back in...
--- utility_install.sh
+++ utility_install.sh
@@ -8,6 +8,11 @@ apt install synaptic -y
 apt install filezilla -y
 
 add-apt-repository ppa:nathan-renniewaldock/flux
+apt update
 apt install fluxgui -y
 
-# need sublime text here...
+# sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
