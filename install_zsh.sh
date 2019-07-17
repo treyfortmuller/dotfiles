fdec4ac9904b4c8c2efbcfee0b3ec008a0237d9a
treyfortmuller <tfortmuller@berkeley.edu>
2019-07-17 10:24:26 -0700

    oh my zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,3 +3,7 @@
 # set up zsh
 sudo apt-get install zsh -y
 chsh -s /bin/zsh
+
+# get oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
