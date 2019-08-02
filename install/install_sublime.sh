e4735117c6ce2bf948c2782663fdb1233fd3298f
Trey Fortmuller <tfortmuller@berkeley.edu>
2019-08-02 09:55:11 -0700

    Update install_sublime.sh

--- install/install_sublime.sh
+++ install/install_sublime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
