1ab802c1f4d28fe32938edf8aea644c08afaa9a5
treyfortmuller <tfortmuller@berkeley.edu>
2019-08-16 10:17:03 -0700

    add miniconda3

---
+++ install/install_miniconda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+bash ./miniconda.sh -b -p ~/miniconda
+rm ./miniconda.sh
+
+echo "PATH=\$PATH:\$HOME/miniconda/bin" >> ~/.bashrc
+echo "PATH=\$PATH:\$HOME/miniconda/bin" >> ~/.zshrc
+
