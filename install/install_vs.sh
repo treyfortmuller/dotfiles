7be83cffb24d7df11e4b905b3787844e7ca7714c
treyfortmuller <tfortmuller@berkeley.edu>
2019-08-18 18:59:20 -0700

    replaced vs code install script

--- install/install_vs.sh
+++ install/install_vs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install software-properties-common apt-transport-https -y
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 
-sudo apt update
-sudo apt install code -y
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+rm ./packages.microsoft.gpg
