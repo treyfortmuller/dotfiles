524ea5e538f67a2b4e28ba70cdc52a2c7ede51e1
treyfortmuller <tfortmuller@berkeley.edu>
2019-09-03 09:26:53 -0700

    wiki startup

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -11,6 +11,7 @@ alias q="qalc"
 alias clear-crash="sudo rm /var/crash/*"
 alias desktops="cd /usr/share/applications"
 alias dots="cd ~/Dev/dotfiles"
+alias wiki="mdbook serve ~/Dev/wiki_trey --open"
 
 # youtube music download script
 alias yt_music="python ~/Dev/youtube_music/yt_music.py"
@@ -23,6 +24,9 @@ alias barc_ft="sftp odroid@10.0.0.1"
 alias v2v_white="ssh ubuntu@white_pi"
 alias v2v_red="ssh ubuntu@red_pi"
 alias v2v_black="ssh ubuntu@black_pi"
+
+# rust
+alias toml="vim ./Cargo.toml"
 
 FILE=~/.aliases.local.zsh
 if test -f "$FILE"; then
