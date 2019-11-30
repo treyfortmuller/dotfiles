f0a83a60b3fc11738a99746f647c46de60254abe
treyfortmuller <tfortmuller@berkeley.edu>
2019-11-30 00:43:30 -0800

    add black python formatter

--- install/install_common.sh
+++ install/install_common.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo apt-get install python-dev python-pip python3-dev python3-pip python3-venv -y
 sudo pip2 install --upgrade pip
 sudo pip3 install --upgrade pip
+pip3 install black --user
 
 # install common utilities
 sudo apt-get install vim htop build-essential curl xclip cmatrix tty-clock qalc tree gnome-tweak-tool neofetch ack -y
