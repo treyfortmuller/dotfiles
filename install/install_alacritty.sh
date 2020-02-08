8e784a0b27146cbcf66b31e1e0f6d919d6735055
treyfortmuller <tfortmuller@berkeley.edu>
2020-02-08 15:50:23 -0800

    install alacritty

--- install/install_alacritty.sh
+++ install/install_alacritty.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# add the terminator ppa
-sudo add-apt-repository ppa:gnome-terminator
+# add the alacritty ppa
+add-apt-repository ppa:mmstick76/alacritty
 sudo apt update
 
-sudo apt install terminator -y
+sudo apt install alacritty -y
 
 # change default terminal emulator
 sudo update-alternatives --config x-terminal-emulator
