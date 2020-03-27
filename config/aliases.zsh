4ad0a6597a048636e8c551fc3070c280bb5c9389
treyfortmuller <tfortmuller@berkeley.edu>
2020-03-26 17:11:56 -0700

    more aliases

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -52,6 +52,20 @@ alias venv="source ./venv/bin/activate" # activate the venv called "venv"
 # xclip
 alias xclip="xclip -selection c"
 
+# Anduril
+alias snk1="cd ~/sources/snk1"
+alias snk2="cd ~/sources/snk2"
+alias snk3="cd ~/sources/snk3"
+alias ainix="cd ~/sources/ainix"
+
+function intlogs {
+    ~/sources/gcs-admin/helictl logs -s interceptord -d $1 -f $2 -u $3
+}
+
+function helilogs {
+    ~/sources/gcs-admin/helictl logs -s dronectrl -d $1 -f $2 -u $3
+}
+
 # Machine-specific aliases
 FILE=~/.aliases.local.zsh
 if test -f "$FILE"; then
